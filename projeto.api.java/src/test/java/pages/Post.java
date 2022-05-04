ba3f6ef817cfd8cbf65103467a554a0af11a5db0
MatheusDEVQA <102087318+MatheusDEVQA@users.noreply.github.com>
2022-05-03 21:57:33 -0300

    Correção no Post Create

--- projeto.api.java/src/test/java/pages/Post.java
+++ projeto.api.java/src/test/java/pages/Post.java
@@ -4,21 +4,21 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
-import org.junit.Test;
+
 
 import io.restassured.http.ContentType;
 
 public class Post {
 	
 	
-	@Test
-	public void metodoPostCreate(String rota) {
+	
+	public void metodoPostCreate(String rota, int status) {
 
 		given().contentType(ContentType.JSON)
 				.body("{\r\n" + "    \"name\": \"Matheus\",\r\n" + "    \"job\": \"QA\"\r\n" + "}").when()
 				.post("https://reqres.in"+ rota)
 				.then()
-				.statusCode(201).assertThat()
+				.statusCode(status).assertThat()
 				.body("name", equalTo("Matheus"));
 
 	}
